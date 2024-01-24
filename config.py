99cf7d46782eec1faa3edf6732a666dcf4606262
RitikKaal <157566142+RitikKaal@users.noreply.github.com>
2024-01-24 12:31:40 +0530

    Espro Music

--- config.py
+++ config.py
@@ -35,9 +35,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Esprorepo/EsproMusic1Bot",
+    "https://github.com/RitikKaal/EsproMusicBot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ritik")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
