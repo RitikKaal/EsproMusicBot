c5167ec52efc2d99c5dfd768973f8887d44ff945
RitikKaal <157566142+RitikKaal@users.noreply.github.com>
2024-02-12 12:17:22 +0530

    Update telegraph.py

--- EsproMusic/plugins/tools/telegraph.py
+++ EsproMusic/plugins/tools/telegraph.py
@@ -1,61 +1,31 @@
 from telegraph import upload_file
 from pyrogram import filters
-import base64
-import httpx
-import os
 from EsproMusic import app
-import pyrogram
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from uuid import uuid4
-from pyrogram import filters
-from pyrogram import Client
-from EsproMusic.utils.inline import close_markup
+from pyrogram.types import InputMediaPhoto
 
-@app.on_message(filters.reply & filters.command(["tgm", "telegraph"]))
-async def upscale_image(client, message):
-    try:
-        if not message.reply_to_message or not message.reply_to_message.photo:
-            await message.reply_text("**ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴄʀᴇᴀᴛ ɪᴛs ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ.**")
-            return
 
-        sent_message = await message.reply_text("**ᴏᴋ ᴡᴀɪᴛ ᴀ sᴇᴄ ᴍᴀᴋɪɴɢ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴏғ ʏᴏᴜʀ ɢɪᴠᴇɴ ᴘɪᴄ ᴡɪᴛʜ ғᴜʟʟ ʜᴅ...**")
+@app.on_message(filters.command(["tgm" , "telegraph"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://telegra.ph" + x
 
-        image = message.reply_to_message.photo.file_id
-        file_path = await client.download_media(image)
+        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
 
-        with open(file_path, "rb") as image_file:
-            f = image_file.read()
+########____________________________________________________________######
 
-        b = base64.b64encode(f).decode("utf-8")
+@app.on_message(filters.command(["graph" , "grf"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://graph.org" + x
 
-        async with httpx.AsyncClient() as http_client:
-            response = await http_client.post(
-                "https://api.qewertyy.me/upscale", data={"image_data": b}, timeout=None
-            )
-
-        with open("upscaled_image.png", "wb") as output_file:
-            output_file.write(response.content)
-
-        # Upload the upscaled image to Telegraph
-        telegraph_url = upload_file("upscaled_image.png")[0]
-
-        # Create caption with the Telegraph link as a button
-        button_text = "๏ ᴏᴘᴇɴ ɪɴ ᴛᴇʟᴇɢʀᴀᴘʜ ๏"
-        button_url = "https://telegra.ph" + telegraph_url
-        reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(button_text, url=button_url)]]
-        )
-
-        await client.send_photo(
-            message.chat.id,
-            photo="upscaled_image.png",
-            caption = f"**➲ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴘʜᴏᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ɪɴ ʜᴅ.**\n\n**๏ ʏᴏᴜ ᴄᴀɴ ᴄᴏᴘʏ ʙʏ ᴄʟɪᴄᴋ ʜᴇʀᴇ: **\n\n**‣**  `{button_url}`\n\n**๏ ᴍᴀᴋᴇᴅ ʙʏ @{app.username}**",
-            reply_markup=reply_markup,
-        )
-
-        # Delete the "Wait making link.." message after sending the results
-        await sent_message.delete()
-
-    except Exception as e:
-        print(f"**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ**: {e}")
-        await message.reply_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ**.")
+        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ {url}')
